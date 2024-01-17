645905c05f13cbef990cc46f1cf62347564ca576
Bill Fenner <fenner@gmail.com>
2024-01-17 12:34:33 -0800

    Initial versions of probe query and response tools

---
+++ probe.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+# Copyright (c) 2023 Arista Networks, Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# This began as a little proof-of-concept, and grew and grew.
+# One of those things that you wouldn't have done it this way
+# if you knew it would have become this from scratch.  But,
+# I'm publishing it anyway, since it does seem useful even
+# if it's awkward and ugly.
+
+from scapy.all import *
+import struct
+import argparse
+
+# Patch scapy to allow sr() to treat type-43 as a reply to type-42.
+origIcmpAnswers = ICMP.answers
+def extendedIcmpAnswers( self, other ):
+    if isinstance( other, ICMP ) and (other.type,self.type) == (42, 43):
+        # id + seq, for extended ICMP echo request and reply
+        return ( other.unused & 0xffffff00 ) == ( self.unused & 0xffffff00 )
+    return origIcmpAnswers( self, other )
+ICMP.answers = extendedIcmpAnswers
+
+codes = {
+          0: 'No Error',
+          1: 'Malformed Query',
+          2: 'No Such Interface',
+          3: 'No Such Table Entry',
+          4: 'Multiple Interfaces Satisfy Query',
+          }
+states = {
+          0: 'Reserved',
+          1: 'Incomplete',
+          2: 'Reachable',
+          3: 'Stale',
+          4: 'Delay',
+          5: 'Probe',
+          6: 'Failed',
+          }
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument( 'ipaddr' )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument( '--ifindex', type=int )
+    group.add_argument( '--ifname' )
+    group.add_argument( '--addr' )
+    parser.add_argument( '--remote', action='store_true' )
+    parser.add_argument( '--checksum', default=-1, type=int )
+    parser.add_argument( '--extra', action='store_true' )
+    parser.add_argument( '--id', default=42, type=int )
+    parser.add_argument( '--seq', default=42, type=int )
+    args = parser.parse_args()
+
+    probeid = args.id
+    seq = args.seq
+    l = 0 if args.remote else 1
+    pkt = IP(dst=args.ipaddr)/ICMP(type=42, unused=( probeid << 16 ) | (seq << 8 ) | l)
+    def icmpPayload( cksum=0 ):
+        data = struct.pack( '>HH', ( 2 << 12 ), cksum ) # ver, checksum
+        if args.ifname:
+            ctype = 1
+            payload = args.ifname.encode()
+            if len( payload ) % 4:
+                payload += b'\x00' * ( 4 - len( payload ) % 4 )
+        elif args.ifindex:
+            ctype = 2
+            payload = struct.pack( '>I', args.ifindex )
+        else:
+            assert args.addr
+            ctype = 3
+            if ':' in args.addr:
+                af = socket.AF_INET6
+            else:
+                af = socket.AF_INET
+            addr = socket.inet_pton( af, args.addr )
+            payload = struct.pack( '>HBB', 1 if af == socket.AF_INET else 2, len(addr), 0 )
+            payload += addr
+        # length = N, Class-Num = iio = 3, C-Type = ctype
+        data += struct.pack( '>HBB', len( payload ) + 4, 3, ctype )
+        return data + payload
+    if args.checksum == -1:
+        # This is a stupid way to calculate and fill in the checksum, but,
+        # I don't feel like replacing bytes in the middle.
+        payload = icmpPayload( checksum( icmpPayload() ) )
+    else:
+        payload = icmpPayload( args.checksum )
+    # One responder says my payload is malformed.  Looking at what *they* send,
+    # it looks similar but with a timestamp appended.  So let's just
+    # append a timestamp like we saw from them.
+    if args.extra:
+        payload += b'e\x83b\x97\x00\x02-\xe3'
+    pkt /= Raw( payload )
+    ans, unans = sr( pkt, filter='icmp[icmptype] == 43', timeout=10 )
+    # for each answer:
+    for a in ans:
+        print( 'Request:' )
+        a[ 0 ][ ICMP ].show()
+        icmp = a[ 1 ][ ICMP ]
+        val = icmp.unused
+        state = ( val & 0xe0 ) >> 5
+        active = bool( val & 0x04 )
+        ipv4 = bool( val & 0x02 )
+        ipv6 = bool( val & 0x01 )
+        print( 'Response:' )
+        icmp.show()
+        print( f"code {icmp.code} ({codes.get(icmp.code,'?')}) state {state} ({states.get(state,'?')}) active {active} ipv4 {ipv4} ipv6 {ipv6}" )
+
+if __name__ == "__main__":
+    main()
+
+# vi:et sw=4
---
+++ proberesponse.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+# Copyright (c) 2024 Arista Networks, Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from scapy.all import *
+import struct
+import argparse
+from collections import namedtuple
+
+# A synthetic PROBE (RFC8335) response generator.
+# We get an ICMP PROBE request, and only parse the
+# L bit.  For each setting of the L bit, we have a
+# series of responses that we iterate through -
+# various code and other bit values - to exercise the
+# code in the probe client that prints out responses.
+# Once we've gone through the list, we time out once
+# and then start the list again from the beginning.
+
+# Local bit = 1: responses have code value, A, 4, 6 bits.
+L1 = namedtuple( 'L1', ['code', 'A', 'v4', 'v6' ] )
+
+# Note: the status field should be zero for all of
+# these responses; should we have a response where
+# we set it to test the client's behavior?
+localResponses = [
+        L1( code=0, A=0, v4=0, v6=0 ),     # interface is not active
+        L1( code=0, A=1, v4=0, v6=0 ),     # curious, interface is active but no v4/v6
+        L1( code=0, A=1, v4=1, v6=0 ),
+        L1( code=0, A=1, v4=0, v6=1 ),
+        L1( code=0, A=1, v4=1, v6=1 ),
+        L1( code=1, A=0, v4=0, v6=0 ),
+        L1( code=2, A=0, v4=0, v6=0 ),
+        L1( code=3, A=0, v4=0, v6=0 ),
+        L1( code=4, A=0, v4=0, v6=0 ),
+        L1( code=5, A=0, v4=0, v6=0 ),     # invalid code
+        L1( code=255, A=0, v4=0, v6=0 ),   # really invalid code
+        L1( code=1, A=1, v4=1, v6=1 ),     # shouldn't happen: code != 0 => A = 0
+        ]
+
+# Local bit = 0: responses have code and state.
+L0 = namedtuple( 'L0', ['code', 'state'] )
+
+# Note: any packet with A, v4, v6 set would be
+# malformed (or, those values should just be ignored);
+# should we test those?
+remoteResponses = [
+        L0( code=0, state=1 ),
+        L0( code=0, state=2 ),
+        L0( code=0, state=3 ),
+        L0( code=0, state=4 ),
+        L0( code=0, state=5 ),
+        L0( code=0, state=6 ),
+        L0( code=0, state=7 ),    # invalid state
+        L0( code=5, state=0 ),    # invalid code
+        L0( code=255, state=0 ),  # really invalid code
+        L0( code=0, state=0 ),    # shouldn't happen, if we found it, state should be nonzero
+        ]
+
+l0idx = 0
+l1idx = 0
+
+iface = None
+
+def respond( pkt ):
+    global l0idx, l1idx
+    print( 'got', repr( pkt ) )
+    response = pkt[ IP ]
+    esrc, edst = pkt.src, pkt.dst
+    pkt.dst, pkt.src = esrc, edst
+    src, dst = response.src, response.dst
+    response.dst, response.src = src, dst
+    response.id += 42
+    response.chksum = None
+    response.len = None
+    response[ ICMP ].type = 43
+    local = response[ ICMP ].unused & 0x1
+    if local:
+        if l1idx >= len( localResponses ):
+            # Last one - let it time out, but reset
+            # so we start the loop again next time
+            l1idx = 0
+            print( "letting it time out" )
+            return
+        resp = localResponses[ l1idx ]
+        l1idx += 1
+        response[ ICMP ].code = resp.code
+        idAndSeq = response[ ICMP ].unused & 0xffffff00
+        if resp.A:
+            idAndSeq |= 0x4
+        if resp.v4:
+            idAndSeq |= 0x2
+        if resp.v6:
+            idAndSeq |= 0x1
+        response[ ICMP ].unused = idAndSeq
+        print( resp )
+    else:
+        if l0idx >= len( remoteResponses ):
+            # Last one - let it time out, but reset
+            # so we start the loop again next time
+            l0idx = 0
+            print( "letting it time out" )
+            return
+        resp = remoteResponses[ l0idx ]
+        l0idx += 1
+        response[ ICMP ].code = resp.code
+        idAndSeq = response[ ICMP ].unused & 0xffffff00
+        idAndSeq |= resp.state << 5
+        response[ ICMP ].unused = idAndSeq
+        print( resp )
+    # recalculate checksums
+    response[ ICMP ].chksum = None
+    print( "sending", repr( pkt ) )
+    sendp( pkt, iface=iface )
+
+def main():
+    global iface
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument( '--iface' )
+    args = parser.parse_args()
+
+    if args.iface:
+        iface = args.iface
+    sniff(iface=iface, filter='icmp[icmptype] == 42', prn=respond)
+
+if __name__ == '__main__':
+    main()
+
+# vi:et sw=4
